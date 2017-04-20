520b352d1be49873e6507ec5eecd09a5890e2ff0
Hack Reactor Student <student@hackreactor.com>
2017-04-20 12:35:24 -0700

    Scraped data displays

--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,2 +1,35 @@
 // Use the code in `archive-helpers.js` to actually download the urls
 // that are waiting.
+var http = require('http');
+var fs = require('fs');
+var request = require('request');
+var archive = require('../helpers/archive-helpers');
+
+// Fetch all the sites we want to upload from archives/sites.txt
+// Loop over array of fetched sites
+ 	// For each instance, get html
+ 	// Create and write new file with html
+// Reset requested pages to zero
+ 
+function addToArchive () {
+	fs.readFile(archive.paths.list, function(err, data) {
+		if (err) throw error;
+		data.toString().split('\n').forEach(function(website) {
+			scraper('https://' + website, function(html) {
+				fs.writeFile(archive.paths.archivedSites + '/' + website, html);
+				console.log('Just added user request "' + website + '" to directory.')
+			});
+		});
+		fs.writeFile(archive.paths.list, '');
+	});
+}
+
+function scraper (url, callback) {
+	request(url, function (error, response, html) {
+	  if (!error && response.statusCode == 200) {
+	    callback(html);
+	  }
+	});
+}
+
+addToArchive();
