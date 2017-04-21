2e6ddd7828189d39c1e82cdc2d580be8e15bd24c
Hack Reactor Student <student@hackreactor.com>
2017-04-20 19:38:38 -0700

    Add cronjob instructions

--- workers/cronjob-fetchTrigger.sh
+++ workers/cronjob-fetchTrigger.sh
@@ -1,11 +1,6 @@
-#!/bin/sh
-/Users/student/.nvm/versions/node/v6.9.5/bin/node /Users/student/Desktop/hrsf75-web-historian/workers/htmlfetcher.js
-./htmlfetcher.js
-
-chmod +x cronjob-htmlfetcher.sh
-
- */01 * * * * /Users/student/Desktop/hrsf75-web-historian/workers/cronjob-fetchTrigger.sh
-
-
-
- /Users/student/.nvm/versions/node/v6.9.5/bin/node
+# Type in command: crontab -e
+# Find the path to node by command: $PATH node
+	# The above will get us to some sub-folder. We need to find the explicit path to the desired folder/application, on the workstation it looked like this: /Users/student/.nvm/versions/node/
+# Once we have the path to node we go into crontab and drag and drop the script after we specify time-interavl for the cronjob. This will allow us to get the path to the folder.
+# For this sprint our command in cronjob looked like this:
+# */01 * * * * /Users/student/.nvm/versions/node/v6.9.5/bin/node /Users/student/Desktop/hrsf75-web-historian    /workers/htmlfetcher.js 
